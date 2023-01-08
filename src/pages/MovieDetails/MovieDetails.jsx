c7983f59a4fd9b0505ca79127f454138254e1b75
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-01-08 19:35:16 +0200

    edits

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,29 +1,32 @@
 import { getMovieDetails } from 'api';
-import { useState } from 'react';
-import { useEffect } from 'react';
+// import { useState } from 'react';
+// import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-export const MovieDetails = () => {
-  const { id } = useParams(); 
-  const [movie, setMovie] = useState([]);
+// export const MovieDetails = () => {
+//   const { id } = useParams(); 
+//   const [movie, setMovie] = useState([]);
     
-  useEffect(() => {
-    async function getMovie() {
-      try {
-        const result = await getMovieDetails(id);
-        setMovie(result);
-      } catch (error) {
-        console.log('Oops! Something went wrong!');
-      }
-    }
-    getMovie();
-  }, [id]);
-  const {title} = movie;
-  return(
-    <div>
-   <button>hello</button>
-  </div>
-    ) 
-};
-
+//   useEffect(() => {
+//     async function getMovie() {
+//       try {
+//         const result = await getMovieDetails(id);
+//         setMovie(result);
+//       } catch (error) {
+//         console.log('Oops! Something went wrong!');
+//       }
+//     }
+//     getMovie();
+//   }, [id]);
+//   const {title} = movie;
+//   return(
+//     <div>
+//    <button>hello</button>
+//   </div>
+//     ) 
+// };
+export const MovieDetails = () => {
+const {id} = useParams();
+ getMovieDetails(id);
+}
 

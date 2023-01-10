1728063c1d67d84bb303a80a39a2c0b61185f250
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-01-10 12:32:24 +0200

    edits

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,8 +1,31 @@
-
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { getMovieReviews } from "api"
 
 
 export const Review = () => {
+const {id} = useParams()
+const[review,setReview] = useState();
 
-return <></>
+useEffect(()=>{
+async function getReview(){
+try {
+    const result = await getMovieReviews(id);
+    setReview(result);
+} catch (error) {
+    
+}
+
+}
+
+getReview();
+},[id])
+
+return <>
+<h1>hello
+    
+</h1>
+
+</>
 
 }
